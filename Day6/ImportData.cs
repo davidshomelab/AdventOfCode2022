f77e759e63e793c92130756957760686bee75390
David Hollings Jahau <10344688+davidshomelab@users.noreply.github.com>
2022-12-06 13:57:25 +0000

    Add day 6

---
+++ Day6/ImportData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class ImportData
+    {
+        internal static char[] GetData(string fileName)
+        {
+            char[] output = System.IO.File.ReadAllText(fileName).ToCharArray();
+
+            return output;
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Day6
+{
+    internal class Program
+    {
+
+        static int FindMarker(char[] message, int markerLength)
+        {
+            for (int i = markerLength; i < message.Length; i++)
+            {
+
+                char[] sectionToCheck = message.Skip(i - markerLength).Take(markerLength).ToArray();
+
+                char[] distinctValues = sectionToCheck.Distinct().ToArray();
+
+                if (distinctValues.Count() == markerLength)
+                {
+                    return i;
+                }
+
+            }
+
+            throw new InvalidOperationException("Message does not contain a marker of the specified length");
+        }
+
+        static void Main(string[] args)
+        {
+            char[] data = ImportData.GetData("input.txt");
+
+            Console.WriteLine($"Part 1: {FindMarker(data,4)}");
+            Console.WriteLine($"Part 2: {FindMarker(data,14)}");
+        }
+    }
+}
